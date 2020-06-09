708967bba4ed19021ef81da7500d70ad98692fee
HectorLopez001 <hector.lopezmendez90@hotmail.com>
2020-06-09 19:11:26 +0200

    Master: First aproach to notifications, virtual and real devices recive notifications in any posible state (but dont work on 2 devices together)

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
  * @format
  */
 
-
+import React from 'react'
 import { AppRegistry } from 'react-native';
 import App from './src';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+
+function HeadlessCheck({}){
+    if(isHeadless){
+        // App has been launched in the background by iOS, ignore
+        return null;
+    }
+
+    return <App />
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
 
 //console.disableYellowBox = true;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,74 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
-
 import cfgStore, { persistor } from './store/configureStore';
-
 import Root from './containers'
+import { fcmService } from './components/FCMService';
+import { localNotificationService } from './components/LocalNotificationService';
+import messaging from '@react-native-firebase/messaging';
 
 const store = cfgStore();
 
-export default class App extends Component {
-    render() {
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+   // alert('Message handled in the background!', remoteMessage);
+});
+
+export default function App() {
+
+    useEffect(() => {
+        fcmService.registerAppWithFCM;
+        fcmService.register(onRegister, onNotification, onOpenNotification)
+        localNotificationService.configure(onOpenNotification);
+
+        function onRegister(token){
+            console.log("[App] onRegister: ", token)
+            //alert("[App] onRegister: ", token)
+        }
+
+        function onNotification(notify){
+            console.log("[App] onNotification: ", notify)
+            //alert("[App] onNotification: ", notify)
+            const options = {
+                soundName: 'default',
+                playSound: true
+            }
+            localNotificationService.showNotification(
+                0,
+                notify.title,
+                notify.body,
+                notify,
+                options
+            )
+        }
+
+        function onOpenNotification(notify){
+            console.log("[App] onOpenNotification: ", notify)
+            //alert("Open Notification: " + notify.body)
+        }
+
+        return () => {
+            console.log("[App] unRegister")
+            //alert("[App] unRegister")
+            fcmService.unRegister()
+            localNotificationService.unRegister()
+        }
+
+    }, [])
+
+            // messaging()
+            //   .getToken()
+            //   .then(token => {
+            //    // return saveTokenToDatabase(token);
+            //    // console.log("token puto:" + token);
+            //    alert("token puto:" + token);
+            //   });
+
         return (
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
                     <Root/>
                 </PersistGate>
             </Provider>
-        )
+        )     
     }
-}
